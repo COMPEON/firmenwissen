9341194d789c0332c6a960fbf6160698ccb88813
Gerrit Seger <gseger@compeon.de>
2018-02-06 15:09:48 +0100

    Separate port and ssl usage semantically

--- lib/firmenwissen/uri_decorator.rb
+++ lib/firmenwissen/uri_decorator.rb
@@ -2,17 +2,21 @@ class URIDecorator < SimpleDelegator
   HTTPS_PORT = 443
   HTTP_PORT = 80
 
-  def derive_port_from_scheme
-    use_ssl? ? HTTPS_PORT : HTTP_PORT
-  end
-
   # The addressable URI implementation does not set this to a default value, but
   # returns nil instead
   def port
     super || derive_port_from_scheme
   end
 
-  def use_ssl?
+  def https?
     scheme == 'https'
+  end
+
+  alias use_ssl? https?
+
+  private
+
+  def derive_port_from_scheme
+    https? ? HTTPS_PORT : HTTP_PORT
   end
 end
