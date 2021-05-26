79a7644586ad76871a954f5cf1f32f6e87e9ed44
Lars Greiving <lgreiving@compeon.de>
2021-05-26 12:52:10 +0200

    Add support for storage of session information

---
+++ lib/firmenwissen/session.rb
@@ -0,0 +1,26 @@
+module Firmenwissen
+  module Session
+    module_function
+
+    def cookies
+      @cookies ||= {}
+    end
+
+    def update_from_set_cookie_headers(set_cookie_headers)
+      return if set_cookie_headers.nil?
+
+      set_cookie_headers.each do |header|
+        name_and_value, _other_attrs = header.split(/;\s?/)
+        name, value = name_and_value.split('=', 2)
+
+        next if name.empty? || value.nil? || value.empty?
+
+        cookies[name] = value
+      end
+    end
+
+    def to_cookie
+      cookies.map { |entry| [entry].join('=') }.join('; ')
+    end
+  end
+end
